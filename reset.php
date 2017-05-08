9f7e6304e8762d9f8f2717ee3b15404c7d19e692
davidmaisch <davidmaisch@a2plcpnl0466.prod.iad2.secureserver.net>
2017-05-08 13:11:07 -0700

    Reset Pass Modifications

--- reset.php
+++ reset.php
@@ -1,22 +1,20 @@
-<?php
+<?php 
 	$hostname = "localhost";
-        $dbloginusername = "root";
-        $dbloginpassword = "replacepass2";
-	$connect = mysqli_connect("$hostname", "$dbloginusername", "$dbloginpassword","MarvelMarks") or die("Could not connect to MySQL database at address " . $hostname . " using provided login credentials!");
-      
- 
-        $to = $_POST['email'];
-        $sql = "ALTER something where email=$to ";
-        $result = mysqli_query($db, $sql);
-        $from = "whatever@email.com";
-        $subject = "Marvel Marks Password Reset";
-        $headers = "From: $from";
-        $msg = "
-                You can reset your password by clicking this link with this temporary password: randomlyGeneratePassword
-                
-                If you did not cause this message to be sent, please contact the administrator."
-      
-	
-  $ok = @mail($to, $subject, $msg, $heaxers, "-f".$from);
-?>
+	$dbloginusername = "root";
+	$dbloginpassword = "replacepass2";
+//	$connect = mysqli_connect($hostname, $dbloginusername, $dbloginpassword, "MarvelMarks");
 
+
+	$to = $_POST['email'];
+	$sql = "alter somethere where";
+//	$result = ($connect, $sql);
+	$from = "admin@olonni.com";
+	$subject = "Marvel Marks Password Reset";
+	$headers = "From: $from";
+	
+	$msg = "Click here";
+//	$ok = @mail($to, $subject, $msg, $headers, "-f".$from);
+	echo "You should recieve an email in 5-10 minutes on instructions to reset your password. You will be redirected back to the home screen.";
+	header("refresh:5; url=index.html");
+
+?>
