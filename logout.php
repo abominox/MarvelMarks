9a023bb097a58a0afd0c845f0860804aa8cd33cc
abominox <pwnagepal@gmail.com>
2020-02-15 09:53:59 -0500

    New project base.

--- logout.php
+++
@@ -1,8 +0,0 @@
-<?php
-	session_start();
-	session_destroy();
-
-	header('Location: index.html');
-	//Debug Line
-	//echo"You've been successfully logged out!" . "<a href='index.html'> Click here to return.</a>";
-?>
